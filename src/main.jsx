e4e07b0f6f18b56b313077d5d81cdaea5feaa399
mariaN <126809356+mariaNtC@users.noreply.github.com>
2024-10-16 14:12:56 -0300

    Update main.jsx
    Corrección de ruta CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import CartProvider from './context/cartProvider.jsx'
+import CartProvider from './context/CartProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
